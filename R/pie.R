e8041543f1feff3d5f0493203d77a3ae21ddd540
jamieRowen <jamie@jumpingrivers.com>
2017-03-20 16:49:12 +0000

    add source for pie widget

---
+++ R/pie.R
@@ -0,0 +1,52 @@
+#' <Add Title>
+#'
+#' <Add Description>
+#'
+#' @import htmlwidgets
+#'
+#' @export
+pie <- function(values, legendPosition = "bottom", width = NULL, height = NULL, elementId = NULL) {
+
+  # forward options using x
+  x = list(
+    values = values,
+    legendPosition = legendPosition
+  )
+
+  # create widget
+  htmlwidgets::createWidget(
+    name = 'pie',
+    x,
+    width = width,
+    height = height,
+    package = 'pieR'
+  )
+}
+
+#' Shiny bindings for pie
+#'
+#' Output and render functions for using pie within Shiny
+#' applications and interactive Rmd documents.
+#'
+#' @param outputId output variable to read from
+#' @param width,height Must be a valid CSS unit (like \code{'100\%'},
+#'   \code{'400px'}, \code{'auto'}) or a number, which will be coerced to a
+#'   string and have \code{'px'} appended.
+#' @param expr An expression that generates a pie
+#' @param env The environment in which to evaluate \code{expr}.
+#' @param quoted Is \code{expr} a quoted expression (with \code{quote()})? This
+#'   is useful if you want to save an expression in a variable.
+#'
+#' @name pie-shiny
+#'
+#' @export
+pieOutput <- function(outputId, width = '100%', height = '400px'){
+  htmlwidgets::shinyWidgetOutput(outputId, 'pie', width, height, package = 'pieR')
+}
+
+#' @rdname pie-shiny
+#' @export
+renderPie <- function(expr, env = parent.frame(), quoted = FALSE) {
+  if (!quoted) { expr <- substitute(expr) } # force quoted
+  htmlwidgets::shinyRenderWidget(expr, pieOutput, env, quoted = TRUE)
+}
